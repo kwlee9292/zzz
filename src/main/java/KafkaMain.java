89b2d389bf54a78284e9a00b8aefd844cd06cfd6
kwlee9292 <kbb5006@gmail.com>
2021-02-18 10:56:30 +0900

    Initial commit

---
+++ src/main/java/KafkaMain.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class KafkaMain {
+    public static void main(String[] args) throws IOException {
+        String topicName = args[1];
+        String brokers = args[2];
+
+        switch(args[0]){
+            case "produce":
+                Producer.produce(brokers, topicName);
+                break;
+            case "consume":
+                Consumer.consume(brokers, topicName);
+                break;
+            default:
+                System.out.println("Wrong arguments");
+                break;
+
+
+        }
+        System.exit(0);
+    }
+
+
+
+}
---
+++ src/main/java/Producer.java
@@ -0,0 +1,28 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.IOException;
+import java.util.Properties;
+
+
+
+public class Producer {
+
+    public static void produce(String brokers, String topicName) throws IOException {
+        // Create Producer
+        KafkaProducer<String, String> producer;
+        // Configure
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", brokers);
+        properties.setProperty("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+        producer = new KafkaProducer<>(properties);
+
+        for(int i=0;i<100;i++){
+            ProducerRecord record = new ProducerRecord<String, String>(topicName, Integer.toString(i));
+            producer.send(record);
+        }
+        producer.close();
+    }
+}
